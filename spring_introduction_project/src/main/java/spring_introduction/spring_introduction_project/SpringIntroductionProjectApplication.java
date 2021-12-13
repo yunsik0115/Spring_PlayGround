2b13efc7d61ee051dca4bff5bb08a6cf6671e2fb
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-13 18:18:05 +0900

    initial spring project

---
+++ spring_introduction_project/src/main/java/spring_introduction/spring_introduction_project/SpringIntroductionProjectApplication.java
@@ -0,0 +1,13 @@
+package spring_introduction.spring_introduction_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionProjectApplication.class, args);
+	}
+
+}
---
+++ spring_introduction_project/src/test/java/spring_introduction/spring_introduction_project/SpringIntroductionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_introduction.spring_introduction_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

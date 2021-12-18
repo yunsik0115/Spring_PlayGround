59a24fa39ef3a199670a8157965896d435e3f82a
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-18 16:11:38 +0900

    JDBC Template Implementation For Dependency Injection to JdbcTemplateMemberRepository Autowiring Datasource in springconfig required

--- spring_introduction_project/src/main/java/spring_introduction/spring_introduction_project/SpringConfig.java
+++ spring_introduction_project/src/main/java/spring_introduction/spring_introduction_project/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring_introduction.spring_introduction_project.repository.JdbcMemberRepository;
+import spring_introduction.spring_introduction_project.repository.JdbcTemplateMemberRepository;
 import spring_introduction.spring_introduction_project.repository.MemberRepository;
 import spring_introduction.spring_introduction_project.repository.MemoryMemberRepository;
 import spring_introduction.spring_introduction_project.service.MemberService;
@@ -28,6 +29,12 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
+
+    /*
+    * 지금은 SpringConfig에서 dataSource를 받아서 JdbcTemplateMemberRepository에 직접 주입하고 있습니다.
+    * 이렇게 직접 주입해도 되고, 말씀하신 것 처럼 직접 주입 받지 않고, @Autowired로 주입 받아도 됩니다. 여러가지 방법이 가능합니다.
+    * 그런데 여기서 JdbcTempalteMemberRepository를 @Bean을 사용해서 수동으로 스프링 빈으로 등록하는데,
+    * dataSource가 생성자의 필수 파라미터 이기 때문에 SpringConfig에서는 dataSource를 꼭 받아와서 넣어주어야 합니다. */
 }
---
+++ spring_introduction_project/src/main/java/spring_introduction/spring_introduction_project/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,68 @@
+package spring_introduction.spring_introduction_project.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import spring_introduction.spring_introduction_project.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired // 생성자 딱 한개면 Autowired 생략 가능하다.
+    public JdbcTemplateMemberRepository(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override // 숙지만 하자
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny(); // Optional로 반환한다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper(){
+        return new RowMapper<Member>() {
+            @Override
+            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return member;
+            }
+        };
+    }
+}

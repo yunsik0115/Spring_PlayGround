6ca2115f2c12daecb6990be9c27c85090e97a9b6
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-18 12:44:16 +0900

    Memory Service Implemented Method Extraction. Java 8 Lambda

---
+++ spring_introduction_project/src/main/java/spring_introduction/spring_introduction_project/service/MemberService.java
@@ -0,0 +1,52 @@
+package spring_introduction.spring_introduction_project.service;
+
+import spring_introduction.spring_introduction_project.domain.Member;
+import spring_introduction.spring_introduction_project.repository.MemberRepository;
+import spring_introduction.spring_introduction_project.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member){
+
+        // 회원 가입시 같은 이름이 있는 경우? (중복?)
+
+        // Optional<Member> result = memberRepository.findByName(member.getName()); 보다는
+        validateDuplicateMember(member);
+        // ctrl alt shift m : extract method
+        /* result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });*/
+        // Optional 내에 Member 객체가 있기 때문에 이런 방식으로 해결 가능 과거에는 try null ...
+        // 꺼내고 싶으면 get으로 꺼낼수있지만 권장 X / Orelseget도 많이 씀
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    // 전체 회원 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll(); // List 멤버형
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+
+
+}

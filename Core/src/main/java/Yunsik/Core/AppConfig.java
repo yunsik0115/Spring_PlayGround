23179348e7d9554c3d5b8d985d99cddf34f45777
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-19 20:13:53 +0900

    AppConfig.java Refactoring For Intuitive Code

--- Core/src/main/java/Yunsik/Core/AppConfig.java
+++ Core/src/main/java/Yunsik/Core/AppConfig.java
@@ -1,5 +1,6 @@
 package Yunsik.Core;
 
+import Yunsik.Core.discount.DiscountPolicy;
 import Yunsik.Core.discount.FixedDiscountPolicy;
 import Yunsik.Core.member.MemberService;
 import Yunsik.Core.member.MemberServiceImpl;
@@ -11,10 +12,18 @@ import Yunsik.Core.order.OrderServiceImpl;
 public class AppConfig {
     // 생성된 객체 인스턴스의 참조를 생성자를 통해서 주입해준다.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository()); // 역할이 잘 드러나도록 Ctrl + Alt + M
+    }
+
+    private MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository(); // 저장소 변경시 해당 코드만 변경하면 된다.
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }

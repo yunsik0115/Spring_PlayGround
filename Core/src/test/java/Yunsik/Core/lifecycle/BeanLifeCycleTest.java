d85d2da827a8f50f2a61818c84e0c63615925694
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-24 19:36:15 +0900

    Spring Bean LifeCycle Intervention By Using InitializingBean, DisposableBean

---
+++ Core/src/test/java/Yunsik/Core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,53 @@
+package Yunsik.Core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        // ConfigurableApplicationContext - ac.close를 사용하기 위함.
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // ApplicationContext를 닫는다.
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
+
+/*
+생성자 호출, url = null
+Connect : null
+Call: null Message = 초기화 연결 메세지
+
+NULL? URL 어디갔나?
+
+--> 실패!
+
+객체를 생성 -> 필요한 값을 Setter로 주입(객체 생성 후 주입)
+객체를 생성하는 단계에는 url이 없고, 객체를 생성한 다음에 외부에서 수정자 주입을 통해서 setUrl() 이 호출되어야 url이 존재하게 된다.
+
+InitializingBean, DisposableBean 사용 시
+생성자 호출, url = null
+Connect : http://hello-spring.dev
+Call: http://hello-spring.dev Message = 초기화 연결 메세지
+19:33:00.027 [main] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@53ce1329, started on Fri Dec 24 19:32:59 KST 2021
+끝나면서 안전하게 disconnect 호출함.
+close : http://hello-spring.dev
+
+인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 다음의 더
+나은 방법들이 있어서 거의 사용하지 않는다.
+
+ */
---
+++ Core/src/test/java/Yunsik/Core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package Yunsik.Core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출되는 url
+
+    public void connect(){
+        System.out.println("Connect : " + url);
+    }
+
+    public void call(String message){
+        System.out.println("Call: "+ url + " Message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close : " + url);
+    }
+
+    @Override // 의존관계 주입이 끝나면 호출 주겠다
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override // bean이 종료될 때 호출된다.
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}

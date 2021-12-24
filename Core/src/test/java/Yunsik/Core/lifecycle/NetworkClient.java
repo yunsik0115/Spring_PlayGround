6c012ede5be813ddf2c3ec4944ee661a0964b690
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-24 19:48:10 +0900

    Spring Bean LifeCycle Intervention By using @PostConstruct, @PreDestory Annotations - Recommended -

--- Core/src/test/java/Yunsik/Core/lifecycle/NetworkClient.java
+++ Core/src/test/java/Yunsik/Core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package Yunsik.Core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /*public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
@@ -66,13 +69,13 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
-
+    @PostConstruct
     public void init() {
         connect();
         call("초기화 연결 메세지");
     }
 
-
+    @PreDestroy
     public void close() {
         disconnect();
     }

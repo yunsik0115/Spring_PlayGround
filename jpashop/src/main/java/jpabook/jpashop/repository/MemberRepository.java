57060826969597f9198f64fc49c6bf5d8d26a276
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-26 20:28:54 +0900

    MemberService Implementation @RequiredArgsConstructor JPA Query(Entity Search) - Several Comments added

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // Spring Bean으로 등록
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    //@PersistenceContext
+    private final EntityManager em;
+
+    /*
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+     */
+
+    public void save(Member member){
+        em.persist(member); // 영속성 컨텍스트에 멤버 객체가 올라감, 그 때 ID가 생성됨.
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class) // -> 엔티티 객체 대상 쿼리, SQL은 테이블 대상 쿼리
+              .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)// JPA의 모든 데이터 변경이나 로직들은 Transactional 내부에서 무조건 실행되어야 함.
+//@AllArgsConstructor
+@RequiredArgsConstructor // final로 된 필드들만 DI 해준다.(LOMBOK)
+public class MemberService {
+
+    private final MemberRepository memberRepository; // field injection 권장 X
+
+    /*@Autowired // 런타임에서 누군가 이걸 바꿀 수 있다. 바꿀 일이 없는데
+    public void setMemberRepository(MemberRepository memberRepository){ // setter injection 권장 X
+        this.memberRepository = memberRepository;
+    }*/
+
+    // 생성자 Injection 권장 TestCase에서도 사용 용이함. => AllArgsConstructor
+    /*@Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }*/
+
+    // 회원 가입
+    @Transactional(readOnly = false)
+    public Long join(Member member){
+
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        // Validation logic 문제 -> WAS 여러개 뜨는데, 동시에 DB에 Insert 요청시 답이 없다 --> MultiThread 사용을 고려해서 데이터베이스에 unique 제약조건을 걸자
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+
+    }
+
+    @Transactional
+    // 회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    @Transactional
+    // 회원 단 건 조회
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+
+}

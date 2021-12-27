700eb0edaf17e6baa10b5bd1d22dd6b9f1be018e
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-27 17:40:18 +0900

    ItemRepository Implementation ItemService Implementation

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            em.persist(item); // JPA 저장 전까지 ID값이 없음, 새로 생성한 객체
+        } else{
+            em.merge(item); // 이미 DB에 등록되어 어디서 가져옴, Update와 비슷함.
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}

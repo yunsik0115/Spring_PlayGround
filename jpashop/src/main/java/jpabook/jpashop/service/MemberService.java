5d0022dbf9dcd91d94e0b07ded9034cc8e18eef0
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-28 18:19:37 +0900

    Member Registration Implementation

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -16,16 +16,20 @@ public class MemberService {
 
     private final MemberRepository memberRepository; // field injection 권장 X
 
-    /*@Autowired // 런타임에서 누군가 이걸 바꿀 수 있다. 바꿀 일이 없는데
+    /*
+    @Autowired // 런타임에서 누군가 이걸 바꿀 수 있다. 바꿀 일이 없는데
     public void setMemberRepository(MemberRepository memberRepository){ // setter injection 권장 X
         this.memberRepository = memberRepository;
     }*/
 
     // 생성자 Injection 권장 TestCase에서도 사용 용이함. => AllArgsConstructor
-    /*@Autowired
+
+    /*
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
-    }*/
+    }
+    */
 
     // 회원 가입
     @Transactional(readOnly = false)
@@ -40,7 +44,8 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member){
         List<Member> findMembers = memberRepository.findByName(member.getName());
-        // Validation logic 문제 -> WAS 여러개 뜨는데, 동시에 DB에 Insert 요청시 답이 없다 --> MultiThread 사용을 고려해서 데이터베이스에 unique 제약조건을 걸자
+        // Validation Logic 문제 -> WAS 여러개 뜨는데, 동시에 DB에 Insert 요청시 답이 없다
+        // --> MultiThread 사용을 고려해서 데이터베이스에 unique 제약조건을 걸자
         if(!findMembers.isEmpty()){
             throw new IllegalStateException("이미 존재하는 회원입니다");
         }

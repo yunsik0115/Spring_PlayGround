d51f3c16efedb3d3abeaf60a6b1fa115e0d66036
Yunsik Kim <yunsik0115@uos.ac.kr>
2021-12-29 23:26:25 +0900

    EL1025E: The collection has '0' elements, index '0' is invalid Error

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -17,7 +17,7 @@ public class ItemRepository {
         if(item.getId() == null){
             em.persist(item); // JPA 저장 전까지 ID값이 없음, 새로 생성한 객체
         } else{
-            // em.merge(item); 이미 DB에 등록되어 어디서 가져옴, Update와 비슷함.
+            em.merge(item); //이미 DB에 등록되어 어디서 가져옴, Update와 비슷함.
             // 왠만하면 쓰지 말자, 일부 필드에 변경이 없거나 변경하지 않아 변수 할당이 아예 되지 않는 경우 NULL이 되는것이고
             // 변수 할당이 안된 필드가 NULL로 바뀌게 되어 복잡한 상황이 발생할 수 있다, 쓰지 말고 최선을 다해 더티체킹을 사용하는것을 권장!
         }

32b1ec173789d8cfaf065452572f1ee092183adf
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-01 20:32:12 +0900

    BI&UNI Directional, Relational Mapping Example

--- JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/JpaMain.java
+++ JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/JpaMain.java
@@ -1,5 +1,7 @@
 package jpabook_jpashop;
 
+import jpabook_jpashop.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
--- JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Order.java
+++ JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook_jpashop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS") // 가끔 order by 예약어 등으로 꼬일때가 있다.
@@ -11,8 +13,15 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member; // 설계할땐 단방향, 개발하다 필요시에만 양방향 사용
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
@@ -27,12 +36,12 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
+    public Member getMember() {
+        return member;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
+    public void setMember(Member member) {
+        this.member = member;
     }
 
     public LocalDateTime getOrderDate() {
@@ -49,5 +58,10 @@ public class Order {
 
     public void setOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
+    }
+
+    public void addOrderItem(OrderItem orderItem){ // 연관관계 편의 메서드
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
     }
 }

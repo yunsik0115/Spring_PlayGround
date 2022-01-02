efcf4f67e858831fbaed7846025c131cc529c63b
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-02 16:09:59 +0900

    Single Table Strategy DB Structure Mapping (TABLE Per Class not recommended Difficult Integration Service)

--- JPA-Demo/src/main/java/hellojpa/Item.java
+++ JPA-Demo/src/main/java/hellojpa/Item.java
@@ -3,7 +3,10 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // Join 전략의 사용, 기본값 = 싱글테이블
+//@Inheritance(strategy = InheritanceType.JOINED) // Join 전략의 사용, 기본값 = 싱글테이블
+@Inheritance(strategy = InheritanceType.JOINED)
+// TABLE PER CLASS - ITEM 테이블이 만들어지지 않고 테이블마다 중복 필드 생성한다, 명확하지 않은 조회시 Union을 통한 모든 테이블 조회함. (비효율적)
+// Table per class는 ORM DBA 둘다 싫어함 나중에 큰 대가를 치르게 된다. -> 새로 추가할 때마다 시스템 변경 필요
 @DiscriminatorColumn //DTYPE 생성
 public class Item {
 

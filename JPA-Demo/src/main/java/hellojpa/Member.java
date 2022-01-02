754ca2bd618ace925067faacac5b03fd105a5c60
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-02 12:30:43 +0900

    1:1 Mapping Debate With DBA Required, Lazy-Loading(Not supported when it must query FK) VS DB Expansion(Not Easy)

--- JPA-Demo/src/main/java/hellojpa/Member.java
+++ JPA-Demo/src/main/java/hellojpa/Member.java
@@ -14,6 +14,10 @@ public class Member {
     //@Column(name="TEAM_ID")
     //private Long teamId;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
 
     public Long getId() {
         return id;

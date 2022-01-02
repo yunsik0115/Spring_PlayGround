3ee2730dcd5b1e775ba42ec5a533064ca218869e
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-02 12:56:04 +0900

    N:N Mapping, should not use Unexpected Query can be sent Seperate it as 1:N -> TABLE <- N:1

--- JPA-Demo/src/main/java/hellojpa/Member.java
+++ JPA-Demo/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -17,6 +19,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> products = new ArrayList<>();
 
 
     public Long getId() {
---
+++ JPA-Demo/src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,17 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    private Product product;
+}

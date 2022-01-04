fa89fe809be68f789e1e6195c60fa5082bbcf3a6
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-04 16:15:24 +0900

    Embedded Type

--- JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Member.java
+++ JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Member.java
@@ -1,60 +1,25 @@
 package jpabook_jpashop.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member{
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO) // 기본값이라 생략해도 됨
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @Column(name = "USERNAME")
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orderList = new ArrayList<>();
+    @Embedded
+    private Period period;
 
-    public Long getId() {
-        return id;
-    }
+    @Embedded
+    private Address address;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 }

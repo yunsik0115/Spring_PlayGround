4fc5ef226ad0fcf2fd8ed2605e819dae959cb06f
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-06 14:08:05 +0900

    Value Embedded Type Mapping - Example

--- JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Address.java
+++ JPA-SHOP-JPA-BASIC/src/main/java/jpabook_jpashop/domain/Address.java
@@ -1,6 +1,7 @@
 package jpabook_jpashop.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -31,5 +32,21 @@ public class Address {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+    // 직접 접근하면 proxy일때 계산이 안되기 때문에 Getter를 이용하여 equals와 hashcode를 생성하는걸 추천함.
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }

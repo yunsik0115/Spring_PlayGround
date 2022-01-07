1070e052db57a9b5dd2b5cbf2ab1ee2af01308c3
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-07 19:04:36 +0900

    Initial Commit For Learning Servlet

---
+++ Servlet/src/test/java/Hello/Servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package Hello.Servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

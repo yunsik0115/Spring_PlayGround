4a3be52ae08b86de50fad3945b81ab43c031f68d
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-09 15:26:40 +0900

    HTTP Response Data with Json

---
+++ Servlet/src/main/java/Hello/Servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,30 @@
+package Hello.Servlet.basic.response;
+
+import Hello.Servlet.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: application/json
+        response.setHeader("content-type", "application/json");
+        response.setCharacterEncoding("utf-8"); // 의미 없는 파라미터.
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+        //{"username":"kim","age":20}
+        String result = objectMapper.writeValueAsString(data);
+        response.getWriter().write(result);
+    }
+}

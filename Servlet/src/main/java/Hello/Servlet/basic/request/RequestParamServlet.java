b346ecbd21b1dd3ec2fee584fc38f4cb0fd42ef4
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-09 13:53:36 +0900

    Get Method Query String Parsing Methods With Servlet

---
+++ Servlet/src/main/java/Hello/Servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,45 @@
+package Hello.Servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 1. 전체 파라미터 조회
+        System.out.println("[전체 파라미터 조회] - start");
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System. out.println(paramName + "=" + request.getParameter(paramName)));
+        // parameter에 대한 username은 하나고 값이 여러 개인 경우 getParameterValues로 꺼내면 된다.
+        // 단 하나의 값일때만 getParameter를 사용한다 (근데 대부분 단일파라미터를 사용한다)
+        // paramName = Key getParameter(paramName) = value
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println("[단일 파라미터 조회] - start");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - end");
+
+        System.out.println("[이름이 같은 복수 파라미터 조회] - start");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+        }
+        System.out.println("[이름이 같은 복수 파라미터 조회] - end");
+
+        response.getWriter().write("ok");
+    }
+
+}

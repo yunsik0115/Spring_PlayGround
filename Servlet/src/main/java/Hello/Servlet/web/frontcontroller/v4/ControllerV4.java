6f1dc4825dc19f008bcee8e4e7810d27be69db33
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-11 17:16:26 +0900

    Controller Refactoring Model Call Method Changed

---
+++ Servlet/src/main/java/Hello/Servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,15 @@
+package Hello.Servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     *
+     * @param paramMap
+     * @param model
+     * @return viewName
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+
+}

595be5635a1690deb60024d67c18e9ef4c6c7a60
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-01-16 19:44:27 +0900

    Product List Implemenation with thymeleaf

---
+++ SpringMVCItemService/src/main/java/Hello/SpringMVCItemService/web/basic/BasicItemController.java
@@ -0,0 +1,41 @@
+package Hello.SpringMVCItemService.web.basic;
+
+import Hello.SpringMVCItemService.domain.Item;
+import Hello.SpringMVCItemService.domain.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    /*@Autowired
+    public BasicItemController(ItemRepository itemRepository){
+        this.itemRepository = itemRepository;
+    }*/
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * Dataset for Test added
+     */
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
